28193fe53c597255026f08202ef2eb9b16134f82
piotrek <piotrek@c5c3376f-6502-0410-8624-d37bed4c2ef7>
2010-01-09 22:58:50 +0000

    tagging 0.11

---
+++ tags/0.11/lib/plugins/extensions/ge_remover.py
@@ -0,0 +1,77 @@
+# -*- coding: UTF-8 -*-
+
+__revision__ = '$Id$'
+
+# Copyright © 2009 Piotr Ożarowski
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published byp
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+
+# You may use and distribute this software under the terms of the
+# GNU General Public License, version 2 or later
+
+import logging
+
+from sqlalchemy.sql import delete, select
+
+from db.tables import movies as movies_table
+from db.tables import movie_tag as movie_tag_table
+from db.tables import movie_lang as movie_lang_table
+from gutils import question
+from plugins.extensions import GriffithExtensionBase as Base
+from sql import update_whereclause
+
+log = logging.getLogger('Griffith')
+
+class GriffithExtension(Base):
+    name = 'Remover'
+    description = _('Removes all currently filtered movies')
+    author = 'Piotr Ożarowski'
+    email = 'piotr@griffith.cc'
+    version = 0.2
+    api = 1
+    enabled = True # TODO: disable it by default
+
+    toolbar_icon = 'gtk-delete'
+
+    def toolbar_icon_clicked(self, widget, movie):
+        if question(_('Are you sure you want to remove %d movies?') % self.app.total):
+            session = self.db.Session()
+
+            # first: remove all dependend data (associated tags, languages, ...)
+            query = select([movies_table.c.movie_id])
+            # FIXME: self.app._search_conditions contains advfilter conditions only (no other filters)
+            query = update_whereclause(query, self.app._search_conditions)
+            query = query.where(movies_table.c.loaned==False) # don't delete loaned movies
+            for movie_entry in session.execute(query):
+                # tags
+                query_movie_tags = delete(movie_tag_table)
+                query_movie_tags = query_movie_tags.where(movie_tag_table.c.movie_id==movie_entry.movie_id)
+                session.execute(query_movie_tags)
+                # languages
+                query_movie_lang = delete(movie_lang_table)
+                query_movie_lang = query_movie_lang.where(movie_lang_table.c.movie_id==movie_entry.movie_id)
+                session.execute(query_movie_lang)
+                # TODO: removing posters if no longer used by another movie?
+
+            # second: remove the movie entries
+            query = delete(movies_table)
+            # FIXME: self.app._search_conditions contains advfilter conditions only (no other filters)
+            query = update_whereclause(query, self.app._search_conditions)
+            query = query.where(movies_table.c.loaned==False) # don't delete loaned movies
+
+            session.execute(query)
+            session.commit()
+
+            self.app.populate_treeview()
